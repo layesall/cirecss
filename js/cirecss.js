e3be2f9070fe5edd362daf0322e3d440644b24a1
Layesall <info.layesall@gmail.com>
2024-07-25 16:00:04 +0200

    add::static css and js file

---
+++ js/cirecss.js
@@ -0,0 +1,10 @@
+/* cirecss/css/cirecss.js  -------------------------------------------
+
+name        : Cire
+description : simple and mobile first stylesheet
+version     : 1.0
+since       : July, 2024
+author      : Layesall
+----------------------------------------------------------------------*/
+
+// const cirecss = document.getElementById('cire')
